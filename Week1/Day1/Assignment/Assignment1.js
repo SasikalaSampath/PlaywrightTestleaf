d50f27316cadee8bdc60636241ecc8ed2f680762
SasikalaSampath <sasikalasampathjob@gmail.com>
2026-08-19 19:14:06 +0530

    Add files via upload
    assigment2

---
+++ Week1/Day1/Assignment/Assignment1.js
@@ -0,0 +1,13 @@
+var genderType ="male"
+
+function printGender(){
+    console.log(genderType);
+     let color= "brown"
+     if(genderType=="female"){
+         var age = 30
+         let  color = "pink"
+         console.log(color);
+     } console.log(age);
+}
+printGender()
+ console.log(genderType);
