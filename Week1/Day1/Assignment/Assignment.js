1b79edab3e5610a816cb8c528995c7caebdad7c1
SasikalaSampath <sasikalasampathjob@gmail.com>
2026-08-19 19:10:06 +0530

    Add files via upload
    day 1 assignment files added

---
+++ Week1/Day1/Assignment/Assignment.js
@@ -0,0 +1,13 @@
+var genderType ="female"
+
+function printGender(){
+    console.log(genderType);
+     let color= "brown"
+     if(genderType=="female"){
+         var age = 30
+         let  color = "pink"
+         console.log(color);
+     } console.log(age);
+}
+printGender()
+ console.log(genderType);
